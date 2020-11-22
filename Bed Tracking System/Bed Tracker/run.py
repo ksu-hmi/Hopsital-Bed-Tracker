f9d46a26541398aef5c97bcb0dea4d8384f35463
Alinapanjwani <apanjwa9@students.kennesaw.edu>
2020-11-22 13:21:06 -0500

    Run.py added to install the requirements

---
+++ Bed Tracking System/Bed Tracker/run.py
@@ -0,0 +1,2 @@
+ #run this in termnal
+ C:/Users/aismaily/AppData/Local/Programs/Python/Python38-32/python.exe -m pip install -U -r C:/Users/aismaily/Documents/GitHub/Hopsital-Bed-Tracker/requirements.txt
