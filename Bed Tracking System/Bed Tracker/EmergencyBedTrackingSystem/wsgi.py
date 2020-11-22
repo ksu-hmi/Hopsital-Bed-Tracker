9449f7b0e60c4609235ab8b18b2f8e324d8c8e7d
Alinapanjwani <apanjwa9@students.kennesaw.edu>
2020-11-21 22:20:21 -0500

    Modification made to "DJANGO_SETTINGS_MODULE"

--- Bed Tracking System/Bed Tracker/EmergencyBedTrackingSystem/wsgi.py
+++ Bed Tracking System/Bed Tracker/EmergencyBedTrackingSystem/wsgi.py
@@ -12,8 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EmergencyBedTrackingSystem.settings")
-
+os.environ["DJANGO_SETTINGS_MODULE", "EmergencyBedTrackingSystem.settings"]
 
 
 application = get_wsgi_application()
